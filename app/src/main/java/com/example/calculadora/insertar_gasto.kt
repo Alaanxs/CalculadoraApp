596cebf11a01c2828462c5a939dc29aea79f54bd
Alaanxs <alanbrira129@gmail.com>
2020-11-09 05:15:47 -0600

    Insertar gastos con GET a php al 100%

---
+++ app/src/main/java/com/example/calculadora/insertar_gasto.kt
@@ -0,0 +1,32 @@
+package com.example.calculadora
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.insertar_gasto_fragment.*
+import kotlinx.android.synthetic.main.insertar_gasto_fragment.view.*
+import org.json.JSONArray
+
+
+class insertar_gasto : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view: View = inflater.inflate(R.layout.insertar_gasto_fragment, container, false)
+
+        return view
+    }
+
+}
